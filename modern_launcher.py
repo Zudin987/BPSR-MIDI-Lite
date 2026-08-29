03d92c18caf10cc9c50e755352643b90d144d77f
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-30 01:16:14 +0800

    Expose advanced timing profile in Lite

--- modern_launcher.py
+++ modern_launcher.py
@@ -8,6 +8,7 @@ from modern_ui import install_modern_ui
 from online_integration import install_online_integration
 from online_search_bridge import install_online_search_bridge
 from online_search_ui_2026 import install_online_search_ui_2026
+from playback_advanced_ui import install_advanced_playback_profile
 from playback_overhaul import install_playback_overhaul
 
 
@@ -23,6 +24,7 @@ install_online_search_ui_2026()
 install_local_search_integration(app)
 install_gaming_runtime_2026(app)
 install_playback_overhaul(app)
+install_advanced_playback_profile(app)
 
 
 if __name__ == "__main__":
