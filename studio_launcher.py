cf5564d6e66da0f839072c78535a8ff1a94e1857
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-05 21:53:52 +0800

    Install responsive Studio playback toolbar

--- studio_launcher.py
+++ studio_launcher.py
@@ -48,6 +48,7 @@ from studio_polish import install_studio_polish
 from studio_spotdl import install_spotdl_band_audio
 from studio_spotdl_fallback import install_spotdl_ytdlp_fallback
 from ui_band_responsive_2026 import install_band_responsive_patch
+from ui_compact_toolbar_2026 import install_compact_toolbar_patch
 from ui_full_overhaul_2026 import install_full_ui_overhaul
 from ui_persistent_library import install_persistent_library
 from ui_product_overhaul_v34 import install_product_ui_overhaul
@@ -95,6 +96,7 @@ install_spotdl_ytdlp_fallback()
 install_responsive_band_audio()
 install_band_audio(app)
 install_full_ui_overhaul(app)
+install_compact_toolbar_patch()
 install_band_responsive_patch()
 
 
